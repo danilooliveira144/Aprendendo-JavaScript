cb41fb25855ad8a734413a61c7aa8ee88c9226ca
Danilo Oliveira <danilooliveira4586@gmail.com>
2026-07-01 20:17:13 -0300

    Adicionando os estudos da auala 01 e 02

---
+++ Aulas/aula01/index.js
@@ -0,0 +1,57 @@
+// let permite que o valor declarado na variável possa ser alterado.
+// let nome = 'Danilo'
+// let sobreNome = 'oliveira'
+// nome = 'joão'
+// console.log(nome, sobreNome)
+
+// const não permite que o valor da variável seja alterado.
+// const nomeConst = 'Danilo'
+// nome = "João"
+// console.log(nomeConst)
+
+// então quando declarar uma variavel com const, deverá se atentar que não mudará o valor.
+// const primeiroNome = 'Danilo'
+// console.log(primeiroNome)
+
+// Tipos de variavéis
+// Primitivo: string, number, boolean, undifined, null
+
+// let nomeItem = 'caneta'
+// let valorItem = 3
+// let disponivelItem = true
+// let corItem
+// let qtdItem = null
+
+// console.log(qtdItem)
+
+// Referência: Objects, array, functions
+
+//Object
+// let caneta = {
+//     precoItem: 2,
+//     corItem: 'verde',
+//     disponivelItem: true
+// }
+// caneta.corItem = 'vermelho'
+
+// console.log(caneta)
+// console.log(caneta.precoItem)
+
+// Array
+// let amigos = ['Danilo', 'Marcos', 'João', 'lucas']
+// amigos[0] = 'Carlos'
+// console.log(amigos[3])
+// console.log(amigos)
+
+// functions
+//                   parametro
+// function statusVenda (status, total){
+//     console.log('transação ' + status + ' Valor total: R$ ' + total)
+// }
+//          argumento
+// statusVenda('aprovado', 20)
+
+// function imc (altura, peso) {
+//     return peso / (altura*altura)
+// }
+// console.log(imc(1.70, 90))
---
+++ Aulas/aula02/index.js
@@ -0,0 +1,37 @@
+// operadores aritiméticos:
+/**
+ * Soma: +
+ * Subtração: -
+ * Multiplicação: *
+ * Divisão: /
+ */
+
+/**
+ * Porcentagem: %
+ * Multiplicação: **
+ * incremento: ++
+ * decremento: --
+ */
+
+// let num1 = 2
+// let num2 = 3
+
+// console.log(num1 + num2)
+
+// let total = num1 + num2
+//          adiciona +1 antes de executar
+// console.log(total++)
+
+//         adiciona +1 e depois executa
+// console.log(++total)
+
+//          executa e depois remove -1
+// console.log(total--)
+
+// operador de atribuição: +=. Funciona com todo os operadores aritiméticos (*, +, -, /)
+
+// let num1 = 3
+
+// num1 += 20
+
+// console.log(num1)
